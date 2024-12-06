f11f97cfa7d7e2f07c341163f7b3ce243db0be0a
Akshat Dwivedi <akshatdwi755@gmail.com>
2024-12-06 17:28:27 +0530

    learn py

---
+++ classes/app.py
@@ -0,0 +1,12 @@
+class Point:
+    def move():
+        print("Moving point")
+    def draw():
+        print("Drawing point")
+
+point1 =Point() 
+point1.x =899
+point1.y = 900
+point1.move
+print(point1.x)
+Point.draw()
--- functions /error.py
+++ functions /error.py
@@ -1,6 +1,10 @@
 # How to handle error in py 
 try:
     age =int(input("your age:"))
+    income = 20000
+    risk = income/age
     print(age)
+except ZeroDivisionError:
+    print("Age cannot be zero")
 except ValueError:
     print('Invalid value')
