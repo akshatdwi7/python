3427dffa1741e8df30b14cd07929bae3a86a3b6b
Akshat Dwivedi <akshatdwi755@gmail.com>
2024-12-10 00:02:59 +0530

    w

---
+++ classes/inheritance.py
@@ -0,0 +1,14 @@
+class Mamal:
+    def walk(self):
+        print("Walking")
+
+
+class Dog(Mamal):
+
+
+
+class Cat(Mamal):
+    print("meow")
+
+     cat.walk()
+
---
+++ osis.py
@@ -0,0 +1,8 @@
+class Person:  # class 
+    def talk(self):{   # method 
+        print("talk")
+    }
+        
+jhon =Person() # object 
+jhon.talk()
+ 
