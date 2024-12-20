649b2068f710f8949d6e042793382df4ba290d6a
Akshat Dwivedi <akshatdwi755@gmail.com>
2024-12-20 12:57:39 +0530

    automation with py

--- automate.py
+++ automate.py
@@ -2,4 +2,13 @@ import openpyxl as xl
 wb =xl.load_workbook('transactions.xlsx')
 sheet = wb['Sheet1']
 cell =sheet.cell(1,1)
-print(cell.value)
+
+for row in range(2,sheet.max_row+1):
+    cell=sheet.cell(row,3)
+    new_price= cell.value * 0.9
+    new_price_cell =sheet.cell(row,4)
+    new_price_cell.value = '$'+ new_price
+    print(new_price_cell)
+#wb.save('transactions2.xlsx')
+
+
